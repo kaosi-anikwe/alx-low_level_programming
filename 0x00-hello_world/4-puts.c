440145bb9fc6ef5007e3541dac7f3908ee90e538
kaosi <anikwehenryasa@gmail.com>
2022-09-09 10:13:02 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main - prints a string to standard output
+ * @string - the string to be printed
+ *
+ * Return: Return 0
+ */
 int main(void)
 {
 	char string[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(string);
-	return(0);
+	return (0);
 }
