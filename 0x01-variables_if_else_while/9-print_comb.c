175d0a5bbd2570ecc8b1b3e54fec16de365b124d
Kaosi Anikwe <anikwehenryasa@gmail.com>
2022-09-10 08:49:13 +0100

    fixed issues in print combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,12 +6,12 @@
 */
 int main(void)
 {
-	char i;
+	int i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar(i);
-		if (i < '9')
+		putchar(i + '0');
+		if (i < 9)
 		{
 			putchar(',');
 			putchar(' ');
