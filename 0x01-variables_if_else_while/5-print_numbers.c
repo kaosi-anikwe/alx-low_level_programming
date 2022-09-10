6e87af4795cedc41db2b48732899e1ec2d6d0916
Kaosi Anikwe <anikwehenryasa@gmail.com>
2022-09-10 08:11:32 +0100

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - evaluates wheter a number is negative or positive
+* @n - a randomly generated number to be checked
+* Return: returns 0 after printing if negative or not
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+
+	putchar('\n');
+	return (0);
+}
