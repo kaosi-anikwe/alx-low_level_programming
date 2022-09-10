d79fb2b0e25efe9131f7909a40beda8963fa82e5
Kaosi Anikwe <anikwehenryasa@gmail.com>
2022-09-10 08:34:52 +0100

    print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - evaluates wheter a number is negative or positive
+* @n - a randomly generated number to be checked
+* Return: returns 0 after printing if negative or not
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar((char) i);
+	}
+	putchar('\n');
+	return (0);
+}
