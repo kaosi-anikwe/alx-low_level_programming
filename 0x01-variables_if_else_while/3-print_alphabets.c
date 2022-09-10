fdcb8a1bdb19f519c0cb0db7332e3b54914e7252
Kaosi Anikwe <anikwehenryasa@gmail.com>
2022-09-10 07:42:50 +0100

    print alphabdts

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - evaluates wheter a number is negative or positive
+* @n - a randomly generated number to be checked
+* Return: returns 0 after printing if negative or not
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar((char) i);
+	}
+
+	int j;
+
+	for (j = 65; j < 91; j++)
+	{
+		putchar((char) j);
+	}
+	putchar('\n');
+	return (0);
+}
