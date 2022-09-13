01d6a8e6bdfc3c16cbc31e2eea815597b6b8a903
Kaosi Anikwe <anikwehenryasa@gmail.com>
2022-09-13 16:00:30 +0100

    0x02 funcitions 0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - evaluates wheter a number is negative or positive
+* @string - the string to be printed to standard ouput
+* Return: returns 0 after printing the string
+*/
+int main(void)
+{
+    char string[] = "_putchar";
+
+    printf("%s\n", string);
+    return 0;
+}
