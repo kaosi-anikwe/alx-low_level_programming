a64122dcafbc578a82a0bda2c62d711140bf3596
Kaosi Anikwe <anikwehenryasa@gmail.com>
2022-09-14 15:49:48 +0100

    betty 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,41 +2,41 @@
 void fibonacci(void);
 /**
  * main - Entry point
- * 
+ *
  * Return: Always returns 0
  */
 int main(void)
 {
-    fibonacci();
-    return (0);
+	fibonacci();
+	return (0);
 }
 
 /**
  * fibonacci - prints first 50 fibonacci numbers from 1 and 2
- * 
+ *
  * Return: return value is void
  */
 void fibonacci(void)
 {
-    long int first = 1;
-    long int second = 2;
-    long int third;
-    int i;
+	long int first = 1;
+	long int second = 2;
+	long int third;
+	int i;
 
-    printf("%ld, ", first);
-    printf("%ld, ", second);
+	printf("%ld, ", first);
+	printf("%ld, ", second);
 
-    for (i = 0; i < 48; i++)
-    {
-        third = first + second;
-        first = second;
-        second = third;
-        
-        printf("%ld", third);
-        if (i == 47)
-            break;
-        putchar(',');
-        putchar(' ');
-    }
-    putchar('\n');
+	for (i = 0; i < 48; i++)
+	{
+		third = first + second;
+		first = second;
+		second = third;
+
+		printf("%ld", third);
+		if (i == 47)
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
 }
