d6b83addc2b43b1712b5a45560902412745fa3f3
Kaosi Anikwe <anikwehenryasa@gmail.com>
2022-09-14 01:39:53 +0100

    8 & 9

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,21 +1,33 @@
 #include "main.h"
 /**
  * jack_bauer - prints all the minutes in a day
- * 
+ *
  * Return: return value is void
  */
 void jack_bauer(void)
 {
-    int hr1, hr2, min1, min2;
+	int hr1, hr2, min1, min2;
 
-    for (hr1 = 0; hr1 <= 2; hr1++)
-    {
-        for (hr2 = 0; hr2 <= 4; hr2++)
-        {
-            for (min1 = 0; min1 <= 5; min1++)
-            {
-                for (min2 = 0)
-            }
-        }
-    }
+	for (hr1 = 0; hr1 <= 2; hr1++)
+	{
+		for (hr2 = 0; hr2 <= 9; hr2++)
+		{
+			for (min1 = 0; min1 <= 5; min1++)
+			{
+				for (min2 = 0; min2 <= 9; min2++)
+				{
+
+					_putchar(hr1 + '0');
+					_putchar(hr2 + '0');
+					_putchar(':');
+					_putchar(min1 + '0');
+					_putchar(min2 + '0');
+					_putchar('\n');
+
+					if (hr1 == 2 && hr2 == 3  && min1 == 5 && min2 == 9)
+						return;
+				}
+			}
+		}
+	}
 }
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * times_table - Prints the 9 times table starting from 0
+ *
+ * Return: return value is void
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			int n = i * j;
+
+			if (n > 9)
+			{
+				_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');
+			} else
+			{
+				_putchar(n + '0');
+			}
+
+			if (j == 9)
+				break;
+
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
