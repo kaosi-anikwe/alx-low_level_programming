293ee80a1ce258459ac43f27a0279e4010bb9337
Kaosi Anikwe <anikwehenryasa@gmail.com>
2022-09-15 10:54:18 +0100

    0x03 debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* positive_or_negative - evaluates wheter a number is negative or positive
+* 
+* Return: return value is void
+*/
+void positive_or_negative(int n)
+{
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	} else if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	} else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+}
