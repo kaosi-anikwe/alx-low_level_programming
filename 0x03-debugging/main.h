5ecd7fab6177eb048054609d9de7188002228e26
Kaosi Anikwe <anikwehenryasa@gmail.com>
2022-09-15 16:20:00 +0100

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,6 +5,15 @@
 
 void positive_or_negative(int i);
 
+
+#endif /* MAIN_H */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 int largest_number(int a, int b, int c);
+
 
 #endif /* MAIN_H */
